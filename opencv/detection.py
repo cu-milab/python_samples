09759e101402cf90a2063a4f7ddc79e7b03955a0
Yuji Yamauchi <yuu@isc.chubu.ac.jp>
2020-12-15 15:56:44 +0900

    Update detection.py

--- opencv/detection.py
+++ opencv/detection.py
@@ -1,15 +1,17 @@
 import cv2
 
-face_cascade_path = "/Users/yuzi5/Anaconda3/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml"
+#学習モデルの読み込み。下記のNAMEにはwiondowsのユーザー名に変更する必要あり。
+face_cascade_path = "/Users/NAME/Anaconda3/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml"
 
 
 face_cascade = cv2.CascadeClassifier(face_cascade_path)
 
 
-img = cv2.imread('people.jpg')
+img = cv2.imread('lenna.png')
 src_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-faces = face_cascade.detectMultiScale(src_gray, 1.1, 3, True, (20, 20))
+#OpenCV4.xより仕様が変わったため修正。
+faces = face_cascade.detectMultiScale(src_gray, scaleFactor=1.1, minNeighbors=3, minSize=(20, 20))
 
 for x, y, w, h in faces:
     cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
